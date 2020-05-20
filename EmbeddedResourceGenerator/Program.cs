f65898d2b501b5e272f71c60fd6f7f5f0c114bda
Alexander Sorokin (Akvelon INC) <v-alsoro@microsoft.com>
2020-05-20 20:14:34 +0300

    Better file search, save result to file

--- EmbeddedResourceGenerator/Program.cs
+++ EmbeddedResourceGenerator/Program.cs
@@ -3,45 +3,62 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace EmbeddedResourceGenerator
 {
     class Program
     {
+        const string DefaultResultFileName = "result.txt";
+
         static void Main(string[] args)
         {
+            string resultFile = DefaultResultFileName;
             string root = Directory.GetCurrentDirectory();
             if (args.Length > 0 && !string.IsNullOrEmpty(args[0]))
             {
                 root = args[0];
                 if (!Directory.Exists(root))
                 {
-                    Console.Error.Write($"Cannot find directory: {root}");
+                    Console.Error.WriteLine($"Could not find directory: {root}");
                     Environment.Exit(1);
                 }
             }
 
-            Console.WriteLine($"Scanning {root}");
-            var files = ScanDir(root);
-        }
-
-        static IEnumerable<string> ScanDir(string dir)
-        {
-            Console.WriteLine($"Starting directory scan: {dir}");
-            List<string> result = new List<string> ();
-
-            foreach (var file in Directory.GetFiles(dir))
+            Console.WriteLine("Scanning directory:");
+            Console.WriteLine(root);
+            var files = Directory.GetFiles(root, "*", SearchOption.AllDirectories);
+            if (files.Count () == 0)
             {
-                Console.WriteLine($"Found file: {file}");
-                result.Add(file);
-            }
-            foreach (var subDir in Directory.GetDirectories(dir))
-            {
-                Console.WriteLine($"Found directory: {dir}");
-                result.AddRange(ScanDir(subDir));
+                Console.Error.WriteLine("Could not find any files.");
+                Environment.Exit(1);
             }
 
-            return result;
+            Console.WriteLine();
+            Console.WriteLine("Found files:");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var result = string.Empty;
+            foreach (var file in files)
+            {
+                result += $"<EmbeddedResource Include=\"{file}\">" + Environment.NewLine;
+                result += $"    <LogicalName>{file}</LogicalName>" + Environment.NewLine;
+                result += "</EmbeddedResource>"                    + Environment.NewLine;
+            }
+
+            if (File.Exists(resultFile))
+            {
+                Console.WriteLine("Result file already exists. Removing...");
+                File.Delete(resultFile);
+            }
+
+            Console.WriteLine($"Writing result to file: {resultFile}");
+            File.WriteAllText(resultFile, result);
+
+            Console.WriteLine();
         }
     }
 }
